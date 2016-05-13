06b8a310abe6e61e676936d933af03ccc90753e0
xusancai1230 <xsc18229467792@163.com>
2016-05-13 15:50:11 +0800

    Longest Substring Without Repeating Characters

---
+++ LongestSubstring.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+/*
+ 
+ Given a string, find the length of the longest substring without repeating characters.
+ //给定一个字符串,查找最长的子串的长度没有重复字符
+ Examples:
+ 
+ Given "abcabcbb", the answer is "abc", which the length is 3.
+ //给一个“abcabcbb”的字串,输出的答案是“abc”,长度是3。
+ Given "bbbbb", the answer is "b", with the length of 1.
+ //给一个“bbbbb”,答案是“b”,长度是1
+ Given "pwwkew"的字符串, the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ //给一个pwwkew”的字符串,答案是“wke”,与3的长度。注意,答案必须是一个字符串,“pwke”子序列而不是子字符串
+
+*/
+
+import UIKit
+
